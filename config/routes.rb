0f37439382255ee33e0fad0a635eafb795926bc5
qazokm <qazokm@users.noreply.github.com>
2017-08-14 11:48:22 +0800

    Add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root to: 'hello_world#index'
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
