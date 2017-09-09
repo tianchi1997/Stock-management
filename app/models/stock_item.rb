0a9c4aaf8dbb554a64fba01e711fced8b7570af9
qazokm <qazokm@users.noreply.github.com>
2017-09-09 16:29:38 +0800

    Update stock item validations

--- app/models/stock_item.rb
+++ app/models/stock_item.rb
@@ -2,5 +2,5 @@ class StockItem < ApplicationRecord
     has_many :items
 
     validates :name, presence: true
-    validates :expires, presence: true
+    validates :expires, inclusion: { in: [ true, false ] }
 end
