788aa640b745e1940b424f72b34f47224905860e
qazokm <qazokm@users.noreply.github.com>
2017-09-10 11:16:14 +0800

    Add tests to group and location model

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,29 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  context "is valid with a name" do
-    group1 = Group.new(name: "Hospital")
-    group2 = Group.new(name: "Bag", group: group1)
-
-    it "without parent" do
-        expect(group1).to be_valid
-    end
-
-    it "with parent" do
-        expect(group2).to be_valid
-    end
+  it "is valid with a name" do
+    expect(Group.new(name: "Hospital")).to be_valid
   end
 
-  context "is invalid without a name" do
-    group1 = Group.new()
-    group2 = Group.new(group: group1)
-
-    it "without parent" do
-        expect(group1).to_not be_valid
-    end
-
-    it "with parent" do
-        expect(group2).to_not be_valid
-    end
+  it "is invalid without a name" do
+    expect(Group.new()).to be_invalid
   end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "with a group" do
+    g = Group.new name: "Hospital"
+
+    it "valid with name" do
+      expect(g.locations.new name: "Building A").to be_valid
+    end
+
+    it "invalid without name" do
+      expect(g.locations.new).to be_invalid
+    end
+  end
+
+  context "without a group" do
+    it "invalid with name" do
+      expect(Location.new name: "Building A").to be_invalid
+    end
+
+    it "invalid without name" do
+      expect(Location.new).to be_invalid
+    end
+  end
 end
