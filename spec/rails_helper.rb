d2e620705e6c890abf4e4651021cbaa3070be265
qazokm <qazokm@users.noreply.github.com>
2017-09-13 16:39:59 +0800

    Add devise support to RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'devise'
 require 'support/factory_girl'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
