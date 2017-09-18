9a74bc88974f2f5250d22ba1be40a1ceff887ef5
Tianchi Ren <tianchi1997@hotmail.com>
2017-09-18 17:48:50 +0800

    made react component display items list

--- app/assets/javascripts/components/stock_take.js.jsx
+++ app/assets/javascripts/components/stock_take.js.jsx
@@ -2,13 +2,45 @@ var StockTake = React.createClass({
   propTypes: {
     location_id: React.PropTypes.number
   },
+  getInitialState() {
+    return { items: [{
+      "id":0,
+      "location_id": null,
+      "name": "name",
+      "description": "description",
+      "created_at": null,
+      "updated_at": null,
+      "url": null
+    }]};
+  },
+  componentDidMount(){
+    //setting the context of 'this'
+    var self = this; 
+    var fetchstr = "/locations/"
+    fetchstr += this.props.location_id
+    fetchstr += ".json"
+    //fetch the items associated with the location
+    fetch(fetchstr)
+      //parse the response to json
+      .then(function(response) { return response.json(); })
+      .then(function(json) {
+        self.setState({
+          items: json
+          },() => {
+            //set a callback to log the change
+            console.log('updated state value', self.state.items)
+          })
+      })
+  },
 
   render: function() {
     return (
       <div>
         <div>Location: {this.props.location_id}</div>
+        <div>Item name: {JSON.stringify(this.state.items.name)} Item description:{JSON.stringify(this.state.items.description)}</div>
       </div>
     );
-  }
+  }    
 });
+
 
