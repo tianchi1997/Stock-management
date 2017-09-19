4aa5eccfb253f9b011729222dec42598b594384a
qazokm <qazokm@users.noreply.github.com>
2017-09-19 20:39:32 +0800

    Reject blank passwords

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :permission_level)
+      params.require(:user).permit(:email, :password, :permission_level).reject{ |_, v| v.blank? }
     end
 end
