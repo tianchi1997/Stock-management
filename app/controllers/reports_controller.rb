23a2068e06c398f410be87d50956fc591f2b5091
Halki Yamauchi <halki@ii.net>
2017-09-20 19:35:40 +0800

    Stock Report Counts Items
    Still not grouped, but getting somewhere.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,14 @@
 class ReportsController < ApplicationController
   def index
+  	@items = Item.all
+  end
+
+  def show
   end
 
   def locations
+  	@item = Location.find(params[:id]).items
+    #@gr_item = Location.find(params[:id]).items.group(:stock_item_id)
   end
 
   def stock_items
