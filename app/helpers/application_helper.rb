37cd94bfe5dda5c95b31440fd5d6b8ceebe640c5
qazokm <qazokm@users.noreply.github.com>
2017-09-21 14:35:48 +0800

    Update location index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,2 @@
 module ApplicationHelper
-  def nested_display(objects, object_display, child_attribute, depth = 0)
-    capture do
-      objects.map do | object |
-        concat send(object_display, object, depth)
-        concat nested_display(object.send(child_attribute), object_display, child_attribute, depth + 1)
-      end.join.html_safe
-    end
-  end
-
-  def nested_div(div, depth)
-    depth.times do
-      div = content_tag(:div, div, class: "nested_display")
-    end
-
-    return div
-  end
 end
