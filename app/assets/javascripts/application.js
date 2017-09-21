5e0d8078712a40ea07906722823e68ebbc3ea120
qazokm <qazokm@users.noreply.github.com>
2017-09-21 16:16:38 +0800

    Fix bootstrap and treeview plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,13 @@
 //= require rails-ujs
 //= require turbolinks
 
+//= require jquery
+
 //= require react
 //= require react_ujs
 //= require components
+
+//= require bootstrap-treeview
 
 //= require_tree .
 
