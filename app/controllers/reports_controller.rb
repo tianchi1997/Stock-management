26bf8569864f7ca8ca7579c08bdec5b6123af192
qazokm <qazokm@users.noreply.github.com>
2017-10-01 23:48:56 +0800

    Use efficient SQL for report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,22 +2,11 @@ class ReportsController < ApplicationController
   authorize_resource class: false
 
   def location
-    #@items=Location.find(params[:id]).items.joins(:location).joins(:stock_item).joins(:item_expiries).where(location: Location.find(params[:id])).group(:stock_item).sum(:count)
-    @items_table = Item.joins(:location).joins(:stock_item).joins(:item_expiries).where(location: Location.find(params[:id])).group("stock_items.id, items.id").order("stock_items.name").pluck("items.id, stock_items.name, SUM(item_expiries.count), items.required, items.order_to")
-
     add_location_breadcrumb Location.find(params[:id])
     add_breadcrumb "Report", location_report_path(params[:id])
-    
-    if params[:order_to]
-     @items_table = Item.joins(:location).joins(:stock_item).joins(:item_expiries).where(location: Location.find(params[:id])).where("order_to != ?",0).group("stock_items.id, items.id").order("stock_items.name").pluck("items.id, stock_items.name, SUM(item_expiries.count), items.required, items.order_to")
-    end
-    #if params[:expiry]
-    #  @location=item.joins(:location).joins(:stock_item).joins(:item_expiries).where(location: Location.find(params[:id]),:expires => (params[:expires]).group(:stock_item).sum(:count)
-    #end
 
-    #@item=@location.items
-    #@gr_item=item.group(:stock_item_id)
-
+    location = Location.find(params[:id])
+    @items = Item.summary_with_order(location.items, "stock_items.name")
   end
 
   def stock_item
