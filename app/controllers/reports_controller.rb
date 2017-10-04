bf53da088fa76e262802879b8e0e90f25c075c03
lachiepb <lachiepb@gmail.com>
2017-10-04 21:55:58 +0800

    minor changes to rpt controller. all commented until further feature addition

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,9 +5,12 @@ class ReportsController < ApplicationController
     @location = Location.find(params[:id])
     add_location_breadcrumb @location
     add_breadcrumb "Report", location_report_path(@location)
-
-    @location_tree = @location.descendants.preload(stock_item_summaries: [:stock_item]).preload(items: [:stock_item]).arrange(order: [:position, :name, :id])
-  # @location_tree = @location.descendants.arange(order: [:position, :name, :id])
+    #Have to figure out how to filter out lines that have total < required in this area. 
+    #if params[:order_to]
+     # @location_tree = @location.descendants.where("stock_item.total<stock_item.required").preload(stock_item_summaries: [:stock_item]).preload(items: [:stock_item]).arrange(order: [:position, :name, :id])
+    #else
+      @location_tree = @location.descendants.preload(stock_item_summaries: [:stock_item]).preload(items: [:stock_item]).arrange(order: [:position, :name, :id])
+   # end
   end
 
   def stock_item
