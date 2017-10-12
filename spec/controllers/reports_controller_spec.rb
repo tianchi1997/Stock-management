fedcb74162c768c48eeb73ce8c361b6f791fc814
lachiepb <lachiepb@gmail.com>
2017-10-12 14:42:03 +0800

    merge

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -2,10 +2,25 @@ require 'rails_helper'
 require 'support/devise'
 
 RSpec.describe ReportsController, type: :controller do
+  login_report
   describe "GET #location" do
     it "returns http success" do
       get :location
       expect(response).to have_http_status(:success)
+    end
+
+    describe "Variable Assignments" do 
+
+      it "assigns @order to to true" do
+        get :location , params: {order_to: true}
+        expect(assigns(:order_to)).to eq(true)
+      end
+
+      it "assigns @location to a new location" do
+        expect(assigns(:location)).to be_a_new(Location)
+      end
+
+
     end
   end
 end
