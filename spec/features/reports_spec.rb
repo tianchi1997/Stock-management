2f849a7059ce5fd487517113d3c96bdb5b9d0305
qazokm <qazokm@users.noreply.github.com>
2017-10-12 14:34:28 +0800

    Install capybara for feature tests

---
+++ spec/features/reports_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature "Reports", type: :feature do
+  before(:each) do
+    login_as(FactoryGirl.create(:user))
+  end
+
+  scenario "Report" do
+    visit "/"
+    expect(page).to have_text("Locations")
+  end
+end
