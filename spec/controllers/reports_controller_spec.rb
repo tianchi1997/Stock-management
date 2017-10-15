13b4074447a35e407243c00f1693f39de470c836
lachiepb <lachiepb@gmail.com>
2017-10-15 13:36:14 +0800

    Reports Controller tests finished

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -9,12 +9,13 @@ RSpec.describe ReportsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
-    describe "Variable Assignments" do 
+    describe "Variable Assignments, Display tree Ture " do 
+      let(:location) {create(:location)}
+
       before(:each) do
         get :location , params: {id: location.to_param, order_to: true, expiries: true, display_tree: true}
+
       end
-      let(:location) {create(:location)}
-     
 
       it "assigns @order to to true" do
         expect(assigns(:order_to)).to be_truthy
@@ -28,9 +29,63 @@ RSpec.describe ReportsController, type: :controller do
       it "assigns @location correctly" do
         expect(assigns(:location)).to eq(Location.find(location.to_param))
       end
-
+      it "assigns @display_tree correctly" do
+        expect(assigns(:display_tree)).to be_truthy
+      end
       it "assigns locations to be location subtree" do
         expect(assigns(:location_tree)).to eq(Location.find(location.to_param).subtree.arrange(order: [:position, :name, :id]))
+      end
+    end
+
+    describe "Variable Assignments, Display tree false " do 
+      let(:location) {create(:location)}
+
+      before(:each) do
+        get :location , params: {id: location.to_param}
+
+      end
+
+      it "assigns @order to to false" do
+        expect(assigns(:order_to)).to be_falsey
+      end
+      it "assigns expiries to be false" do
+        expect(assigns(:expiries)).to be_falsey
+      end
+      it "assigns @global to be false" do
+        expect(assigns(:global)).to be_falsey
+      end
+      it "assigns @location correctly" do
+        expect(assigns(:location)).to eq(Location.find(location.to_param))
+      end
+      it "assigns @display_tree correctly" do
+        expect(assigns(:display_tree)).to be_falsey
+      end
+    end
+    
+    describe "Global Report variable assignments" do
+      let(:location) {create(:location)}
+
+      before(:each) do
+        get :location , params: {display_tree: true}
+
+      end
+      it "assigns @order to to false" do
+        expect(assigns(:order_to)).to be_falsey
+      end
+      it "assigns expiries to be false" do
+        expect(assigns(:expiries)).to be_falsey
+      end
+      it "assigns @global to be true" do
+        expect(assigns(:global)).to be_truthy
+      end
+      it "assigns @display_tree correctly" do
+        expect(assigns(:display_tree)).to be_truthy
+      end
+      it "assigns @stock_item_summaries to be summary"do
+        expect(assigns(:stock_item_summaries)).to eq(StockItemSummary.where(location: nil).preload(:stock_item))
+      end
+      it "assigns locations to be global tree" do
+        expect(assigns(:location_tree)).to eq(Location.all.arrange(order: [:position, :name, :id]))
       end
     end
   end
