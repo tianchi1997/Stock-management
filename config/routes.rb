143627d3a395b2fabfc56fc42874b600d65052a0
qazokm <qazokm@users.noreply.github.com>
2017-10-17 14:50:38 +0800

    Add comments to views

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Home page
   root to: 'home#index'
   get 'home/index'
 
+  # Login routing
   devise_for :users
 
+  # Item routes
   resources :items, except: [:index]
-  resources :locations
-  resources :stock_items
-  resources :users
-
   post 'items/:id/save_expiries', to: 'items#save_expiries'
 
+  # Location routes
+  resources :locations
+  get 'locations/:parent_id/new', to: 'locations#new', as: 'new_sublocation'
   get 'locations/:id/stock_take', to: 'stock_take#location', as: 'stock_take'
   get 'locations/:id/report', to: 'reports#location', as: 'location_report'
-  get 'locations/:parent_id/new', to: 'locations#new', as: 'new_sublocation'
-  get 'report', to: 'reports#location', as: 'report'
+
+  resources :stock_items # Stock item route
+  resources :users # User administration route
+  get 'report', to: 'reports#location', as: 'report' # Global report route
 end
